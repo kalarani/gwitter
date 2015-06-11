6bbca56c773ffea5fb2c1379d504a51114ece9e6
Kalarani <lkalarani@gmail.com>
2015-06-11 17:26:20 +0530

    Added cron job to poll for new contributions every hour

--- gwitter.js
+++ gwitter.js
@@ -7,14 +7,13 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
 	Migrations.migrateTo('latest');
-	// var twDevs = GHApi.getPublicMembersOf("TWChennai");
-	// twDevs.forEach(function(dev){
-	// 	GHApi.contributionsFrom(dev.login);
-	// });
-	// new Cron(function(){
-	// 	// Chores.update({}, {$set: {done: false}}, {multi: true});
-	// 	console.log("running every hour????");
-	// }, {minute : 0});
+	new Cron(function(){
+		console.log("fetching latest contributions");
+		var twDevs = GHApi.getPublicMembersOf("TWChennai");
+		twDevs.forEach(function(dev){
+			GHApi.contributionsFrom(dev.login);
+		});
+	}, {minute : 0});
   });
 
   Meteor.publish('contributions', function() {
